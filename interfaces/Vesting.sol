62a23c871c5eca555f7582e5ec3b4dbcc7028089
Vlad <vladislav.varadinov@gmail.com>
2023-11-06 14:06:24 +0200

    fix: refactor folder structure

--- interfaces/Vesting.sol
+++ interfaces/Vesting.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.8.18;
 
-import "./Types.sol";
+import "../common/Types.sol";
 
 /// @dev The VestingI contract's address.
 address constant VESTING_PRECOMPILE_ADDRESS = 0x0000000000000000000000000000000000000803;
