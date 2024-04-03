31696f56940573091eff2c687c383fb928264bc1
MalteHerrmann <malte@evmos.org>
2024-04-03 14:33:23 +0200

    move all interfaces back into the contracts directory to get the correct typescript bindings built

--- contracts/SCVesting.sol
+++ contracts/SCVesting.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.8.18;
 
-import "../interfaces/VestingI.sol";
+import "./VestingI.sol";
 
 contract SCVesting {
     receive() external payable {
